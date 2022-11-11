e9ff352b2b819e822eb6aba47a3d0cd7e930b676
nityanandagohain <nityanandagohain@gmail.com>
2022-11-11 14:02:17 +0530

    logger added to print traceId and spanId

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Logger } from 'nestjs-pino';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly logger: Logger) { }
 
   @Get()
   getHello(): string {
+    this.logger.log('Calling getHello()', AppController.name);
     return this.appService.getHello();
   }
 }
