f7a5d478570c3082eaf2d4c9bd795273c4624b46
makeavish <makeavish786@gmail.com>
2023-04-28 15:44:38 +0530

    update otel versions and add attribute and exception

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { context, trace } from '@opentelemetry/api';
 
 @Injectable()
 export class AppService {
   getHello(): string {
+    // Get the current span from the tracer
+    const span = trace.getSpan(context.active());
+    // recordException converts the error into a span event.
+    span.setAttribute('test', true);
+    span.recordException(new Error('This is a test error'));
     return 'Hello World!';
   }
 }
